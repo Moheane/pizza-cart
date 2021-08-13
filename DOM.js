0be1d0c1393632285277e66f5df0d123231a1aac
Ramahadi Jan <moheanerj@gmail.com>
2021-08-13 15:07:09 +0200

    completed large functionalities

--- DOM.js
+++ DOM.js
@@ -1,8 +1,10 @@
 let small = 15
 let medium = 60
-let large = 36
+let large = 99
 
 let currentTotal = 0
+let currentTotal1 = 0
+let currentTotal2 = 0
 
 let currentStringSet = document.querySelector(".smallCurrent")
 let small_plus = document.querySelector(".small_plus")
@@ -39,21 +41,63 @@ let refresh_medium = document.querySelector(".refresh_medium")
 
 medium_plus.addEventListener("click", function () {
     let curr = currentStringSetMedium.value
-    currentTotal+=medium
-    currentStringSetMedium.innerHTML = currentTotal
-    console.log(currentTotal)
+    currentTotal1+=medium
+    currentStringSetMedium.innerHTML = currentTotal1
+
     
 })
 
 medium_minus.addEventListener("click", function () {
     let curr = currentStringSetMedium.value
-    currentTotal-=medium
-    currentStringSetMedium.innerHTML = currentTotal
-    console.log(currentTotal)
+    currentTotal1-=medium
+    currentStringSetMedium.innerHTML = currentTotal1
+ 
     
 })
 
 refresh_medium.addEventListener("click", function () {
     currentStringSetMedium.innerHTML = 0
     
+})
+
+
+
+let currentStringSetlarge = document.querySelector(".largeCurrent")
+let large_plus = document.querySelector(".large_plus")
+let large_minus = document.querySelector(".large_minus")
+let refresh_large = document.querySelector(".refresh_large")
+let totalCkeckout = document.querySelector(".totalCkeckout")
+let checkout = document.querySelector(".checkout")
+
+
+
+large_plus.addEventListener("click", function () {
+    let curr = currentStringSetlarge.value
+    currentTotal2+=large
+    currentStringSetlarge.innerHTML = currentTotal2
+
+    
+})
+
+large_minus.addEventListener("click", function () {
+    let curr = currentStringSetlarge.value
+    currentTotal2-=large
+    currentStringSetlarge.innerHTML = currentTotal2
+   
+    
+})
+
+refresh_large.addEventListener("click", function () {
+    currentStringSetlarge.innerHTML = 0
+    
+})
+
+let check_small = document.querySelector(".mediumCurrent")
+let check_medium = document.querySelector(".largeCurrent")
+let check_large = document.querySelector(".refresh_medium")
+
+
+checkout.addEventListener("click", function () {
+    var totalCheck = check_small.value + check_medium.value + check_large.value
+    totalCkeckout.innerHTML = totalCheck
 })
